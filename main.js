893c2cfda7a940e5f619444ca71bccacb532023c
Samuel Curry <currysl@ucmail.uc.edu>
2019-07-09 12:26:22 -0400

    added animations

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ let mainState = {
   preload: function () {
     game.load.image('bird', 'assets/bird.png');
     this.pipes = game.add.group();
+    game.load.audio('jump', 'assets/jump.wav');
+    game.load.image('pipe', 'assets/pipe.png');
   },
   create: function () {
+    this.jumpSound = game.add.audio('jump');
     game.stage.backgroundColor = '#71c5cf';
     game.physics.startSystem(Phaser.Physics.ARCADE);
     this.bird = game.add.sprite(100, 245, 'bird');
@@ -18,19 +21,27 @@ let mainState = {
     this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
   },
   update: function() {
+    if (this.bird.angle < 20)
+      this.bird.angle += 1;
     if (this.bird.y < 0 || this.bird.y > 490)
       this.restartGame();
     game.physics.arcade.overlap(
-        this.bird, this.pipes, this.restartGame, null, this);
+        this.bird, this.pipes, this.hitPipe, null, this);
   },
   jump: function() {
+    if (this.bird.alive == false)
+      return;
+    this.jumpSound.play();
     this.bird.body.velocity.y = -350;
+    let animation = game.add.tween(this.bird);
+    animation.to({angle: -20}, 100);
+    animation.start();
   },
   restartGame: function() {
     game.state.start('main');
   },
   addOnePipe: function(x, y){
-    const pipe = game.add.sprite(x,y, 'pipe')
+    const pipe = game.add.sprite(x,y, 'pipe');
     this.pipes.add(pipe);
     game.physics.arcade.enable(pipe);
     pipe.body.velocity.x = -200;
@@ -44,6 +55,15 @@ let mainState = {
         this.addOnePipe(400, i * 60 + 10);
     this.score += 1;
     this.labelScore.text = this.score;
+  },
+  hitPipe: function() {
+    if (this.bird.alive == false)
+      return;
+    this.bird.alive = false;
+    game.time.events.remove(this.timer);
+    this.pipes.forEach(function(p){
+      p.body.velocity.x = 0;
+    }, this);
   },
 };
 
