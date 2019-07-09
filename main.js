df3e19c148b19ea77088dea947eb5a280b59f39b
Samuel Curry <currysl@ucmail.uc.edu>
2019-07-09 12:20:01 -0400

    flappy bird v1

---
+++ main.js
@@ -0,0 +1,52 @@
+let mainState = {
+  preload: function () {
+    game.load.image('bird', 'assets/bird.png');
+    this.pipes = game.add.group();
+  },
+  create: function () {
+    game.stage.backgroundColor = '#71c5cf';
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.bird = game.add.sprite(100, 245, 'bird');
+    game.physics.arcade.enable(this.bird);
+    this.bird.body.gravity.y = 1000;
+    let spaceKey = game.input.keyboard.addKey(
+        Phaser.Keyboard.SPACEBAR);
+    this.score = 0;
+    this.labelScore = game.add.text(20, 20, "0",
+        { font: "30px Arial", fill: "#ffffff" });
+    spaceKey.onDown.add(this.jump, this);
+    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+  },
+  update: function() {
+    if (this.bird.y < 0 || this.bird.y > 490)
+      this.restartGame();
+    game.physics.arcade.overlap(
+        this.bird, this.pipes, this.restartGame, null, this);
+  },
+  jump: function() {
+    this.bird.body.velocity.y = -350;
+  },
+  restartGame: function() {
+    game.state.start('main');
+  },
+  addOnePipe: function(x, y){
+    const pipe = game.add.sprite(x,y, 'pipe')
+    this.pipes.add(pipe);
+    game.physics.arcade.enable(pipe);
+    pipe.body.velocity.x = -200;
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+  },
+  addRowOfPipes: function() {
+    const hole = Math.floor(Math.random() * 5) + 1;
+    for (let i = 0; i < 8; i++)
+      if (i !== hole && i !== hole + 1)
+        this.addOnePipe(400, i * 60 + 10);
+    this.score += 1;
+    this.labelScore.text = this.score;
+  },
+};
+
+let game = new Phaser.Game(400, 490);
+game.state.add('main', mainState);
+game.state.start('main');
