6af4669f9c46bb68890481a3e2d10d3c97070914
Samuel Curry <samc735@gmail.com>
2019-07-09 16:17:25 -0400

    Update main.js

--- main.js
+++ main.js
@@ -43,6 +43,7 @@ let mainState = {
   },
   addOnePipe: function(x, y){
     const pipe = game.add.sprite(x,y, 'pipe');
+    pipe.scale.set(window.devicePixelRatio,  window.devicePixelRatio);
     this.pipes.add(pipe);
     game.physics.arcade.enable(pipe);
     pipe.body.velocity.x = -200;
